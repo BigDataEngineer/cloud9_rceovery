fde98225f974f7f686a8c3aace592b88dab97d19
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-02-16 09:45:01 +0000

    1708076701

--- SplunkArchitectLab/alarmLongRunnungInstance.sh
+++ SplunkArchitectLab/alarmLongRunnungInstance.sh
@@ -2,8 +2,8 @@
 
 hoursSinceUp=$(uptime |awk '{ print $3 }'|awk -F ',' '{ print $1 }'|awk -F ':' '{ print $1 }')
 minsSinceUp=$(uptime |awk '{ print $3 }'|awk -F ',' '{ print $1 }'|awk -F ':' '{ print $1 }')
-if [[ $hoursSinceUp -ge 99 ]] || [[ $minsSinceUp -ge 30 ]]
+if [[ $hoursSinceUp -ge 1 ]] || [[ $minsSinceUp -ge 30 ]]
 then
-##aws sns publish --topic-arn arn:aws:sns:us-east-1:985829783879:aws_budget_ecd8d73d-91f0-4791-a03b-978e6afde7f3 --message "Stop EC2 instance $(hostname)"
+aws sns publish --topic-arn arn:aws:sns:us-east-1:985829783879:aws_budget_ecd8d73d-91f0-4791-a03b-978e6afde7f3 --message "Stop EC2 instance $(hostname)"
 echo "test"
 fi
