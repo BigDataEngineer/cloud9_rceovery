341a321770dd73d0f37b8688187593e8da1abc37
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-02-24 07:30:02 +0000

    1708759802

---
+++ py4e/word_histogram.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+            
+print(counts)            
+    
