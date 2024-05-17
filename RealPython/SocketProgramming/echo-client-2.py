5c61999c02583462f8c923bd28243cb9d578359f
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-05-17 07:30:01 +0000

    1715931001

--- RealPython/SocketProgramming/echo-client-2.py
+++ RealPython/SocketProgramming/echo-client-2.py
@@ -5,7 +5,7 @@ PORT=65432
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
-    s.send(b"Hello, World")
+    s.send(b"Hello, Worlyyyyyyyyyyyyyyyyyyyyyyyyyd")
     data=s.recv(1024)
     
 #print(f"{data}!r")    
