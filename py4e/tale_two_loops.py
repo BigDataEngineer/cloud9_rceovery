7f6ae4ca961ad582efac670842c6753063e42263
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-11-21 05:45:01 +0000

    1732167901

---
+++ py4e/tale_two_loops.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+friends = ['Joseph', 'Glen', 'Sally']
+for friend in friends:
+    print('Happy New Year:', friend)
+"""
+
+friends = ['Joseph', 'Glen', 'Sally']
+for i in range(len(friends)):
+    friend=friends[i]
+    print('Happy New Year!!!', friend)
