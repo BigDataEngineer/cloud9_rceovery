293cdc2d00d31c714bbb5de8820fde961442c60b
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-11-24 21:00:01 +0000

    1732482001

--- BigO/print_pair_with_sum_1.py
+++ BigO/print_pair_with_sum_1.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def print_pair_with_sum_1(array, target):
+    counter=0
     for a in array:
         for b in array:
+            counter=counter+1
+            print('evaluating', a, b)
+            print('counter', counter)
             if a+b==target:
+                print('Match!!')
                 print(a,b)
--- BigO/print_pairs_with_sum.py
+++ BigO/print_pairs_with_sum.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 def print_pairs_with_sum(array, target):
+    counter=0
     for left in range(len(array)):
         for right in range(left+1, len(array)):
             a = array[left]
             b = array[right]
+            counter=counter+1
+            print('Evaluating',a,b)
+            print('Counter',counter)
             if a + b == target:
+                print('Match!')
                 print(a, b)
