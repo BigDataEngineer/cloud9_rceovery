e34f79c25d6c761fe8ef14d5d8364c6e58584ce4
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-11-25 23:45:02 +0000

    1732578302

---
+++ BigO/tmp.py
@@ -0,0 +1,45 @@
+for x in [0,2,3,5]:
+    counter=1
+    print(counter)
+    print(x)
+    counter=counter+1
+
+
+"""
+>>> counter=0
+>>> for x in [0,2,3,5]:
+...     counter=counter+1
+...     print(counter)
+...     print(x)
+
+
+
+>>> for x in [0,2,3,5]:
+...     counter=1
+...     print(counter)
+...     print(x)
+...     counter=counter+1
+... 
+1
+0
+1
+2
+1
+3
+1
+5
+>>> counter=0
+>>> for x in [0,2,3,5]:
+...     counter=counter+1
+...     print(counter)
+...     print(x)
+... 
+1
+0
+2
+2
+3
+3
+4
+5
+"""
