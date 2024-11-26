5b8955acf12e58f37a450c1997025d755e59f4e5
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-11-26 02:15:02 +0000

    1732587302

--- py4e/avgnum.1.py
+++ py4e/avgnum.1.py
@@ -3,14 +3,15 @@
 list1=[]
 
 while True:
-    inp=input('Enter any number/done when finished')
+    inp=input('Enter any number/done when finished:')
     if inp=='done':
         break
     value=float(inp)
-    list1.append(inp)
+    list1.append(value)
+    
 print('list',list1)
-avg=float(sum(list1)/len(list1))
+avg=(sum(list1)/len(list1))
 print('list',list1)
 print('list length',len(list1))
 print('list sum',sum(list1))
-print('list avg',sum(avg))
+print('list avg',avg)
