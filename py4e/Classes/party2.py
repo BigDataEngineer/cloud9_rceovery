d093d99f76f7808a298e2396e9dda62edee7d1ba
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-11-27 10:45:02 +0000

    1732704301

---
+++ py4e/Classes/party2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class PartyAnimal:
+    def __init__(self):
+        self.x=0
+        
+    def party(self):
+        self.x=self.x+1
+        print(self,"so far",self.x)
+
+an=PartyAnimal()
+an.party()
+PArtyAnimal.party(an)
+        
+            
+    
