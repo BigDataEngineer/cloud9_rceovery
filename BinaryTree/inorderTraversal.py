c355441781c0e016537f733205e214a4c811b42c
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-12-03 03:45:01 +0000

    1733197501

--- BinaryTree/inorderTraversal.py
+++ BinaryTree/inorderTraversal.py
@@ -15,25 +15,34 @@ class TreeNode:
 def printInorder(node):
     if node is None:
         return
-
-    # First recur on left subtree
-    printInorder(node.left)
+    else:
+        # First recur on left subtree
+        printInorder(node.left)
 
     # Now deal with the node
-    print(node.value, end=' ')
+        print(node.value, end=' ')
 
     # Then recur on right subtree
-    printInorder(node.right)
+        printInorder(node.right)
 
 # Driver code
 if __name__ == '__main__':
     root = TreeNode(1)
 #    root.left = TreeNode(2)
-    root.right = TreeNode(3)
+    root.left = TreeNode(3)
+    root.left.left = TreeNode(2)
+    root.left.right = TreeNode(22)
+    root.left.right.left = TreeNode(1) 
+    root.right = TreeNode(7)
+    root.right.left = TreeNode(None)
+    root.right.right = TreeNode(10)
+    root.right.right.left = TreeNode(11)
+    root.right.right.right = TreeNode(None)
+    
 #    root.left.left = TreeNode(4)
 #    root.left.right = TreeNode(5)
 #    root.right.right = TreeNode(6)
-    root.right.left = TreeNode(5)
+#    root.right.left = TreeNode(5)
     # Function call
     print("Inorder traversal of binary tree is:")
     printInorder(root)
