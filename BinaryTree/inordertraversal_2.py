4613f84137e99bc45416004b25f08798eeb7d5c1
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-12-03 04:15:02 +0000

    1733199302

---
+++ BinaryTree/inordertraversal_2.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self,value=0,left=None,right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        
+def inorderTraversal(node):
+    if node is None:
+        return
+    else:
+        inorderTraversal(node.left)
+        print(node.value, end=' ')
+        inorderTraversal(node.right)
+        
+if __name__ == '__main__':
+    root = Node(1)
+    root.left = Node(3)
+    root.left.left = Node(2)
+    root.left.right = Node(22)
+    root.left.right.left = Node(1) 
+    root.right = Node(7)
+#    root.right.left = TreeNode(None)
+    root.right.right = Node(10)
+    root.right.right.left = Node(11)
+#    root.right.right.right = TreeNode(None)
+    inorderTraversal(root)
+    
