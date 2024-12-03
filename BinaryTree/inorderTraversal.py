3c4ff9ab081ef967f85c78b1dcc0186650c5b6a2
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-12-03 00:15:01 +0000

    1733184901

---
+++ BinaryTree/inorderTraversal.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+What is in a BST?
+root, left child, right child
+define a blueprint/template for a node
+"""
+
+class TreeNode:
+    def __init__(self, value=0,left=None, right=None):
+        self.value=value
+        self.left=left
+        self.right=right
+
+   # Function to print inorder traversal
+def printInorder(node):
+    if node is None:
+        return
+
+    # First recur on left subtree
+    printInorder(node.left)
+
+    # Now deal with the node
+    print(node.data, end=' ')
+
+    # Then recur on right subtree
+    printInorder(node.right)
+
+# Driver code
+if __name__ == '__main__':
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.right = TreeNode(6)
+
+    # Function call
+    print("Inorder traversal of binary tree is:")
+    printInorder(root)
