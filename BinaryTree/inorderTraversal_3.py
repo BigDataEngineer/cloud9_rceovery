b2915c678e2effcd5c7685b86255977967fcee14
goelt2000@gmail.com <ec2-user@ip-172-31-0-215.ec2.internal>
2024-12-08 04:00:01 +0000

    1733630401

--- BinaryTree/inorderTraversal_3.py
+++ BinaryTree/inorderTraversal_3.py
@@ -23,13 +23,13 @@ def inorderTraversal(node, level=1):
 if __name__ == '__main__':
     root = Node(1)
     root.left = Node(3)
-    root.left.left = Node(2)
-    root.left.right = Node(22)
-    root.left.right.left = Node(1) 
+#    root.left.left = Node(2)
+#    root.left.right = Node(22)
+#    root.left.right.left = Node(1) 
     root.right = Node(7)
 #    root.right.left = TreeNode(None)
-    root.right.right = Node(10)
-    root.right.right.left = Node(11)
+#    root.right.right = Node(10)
+#    root.right.right.left = Node(11)
 #    root.right.right.right = TreeNode(None)
     inorderTraversal(root)
     
