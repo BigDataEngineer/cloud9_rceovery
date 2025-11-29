ac9b55254b84b8f8ce55403dc155af24d8da042a
Tushar Goel <goelt2000@gmail.com>
2025-11-29 17:29:20 -0600

    alpha_vantage_globalquote_func, Python_Word_Histogram_Complexity_Comparison

--- py4e/Nov15_2025/alpha_vantage_globalquote_func.py
+++ py4e/Nov15_2025/alpha_vantage_globalquote_func.py
@@ -11,36 +11,30 @@ MY_API_KEY = os.environ.get("MY_API_KEY")
 url='https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=NFLX&apikey=MY_API_KEY'
 port=443
 
-mysocket=socket.socket()
-mysocket.connect(('alphavantage.co',443))
+
+class Solution:
+    def fetch_quote(self):
+        mysocket=socket.socket()
+        mysocket.connect(('alphavantage.co',443))
 
 
-context=ssl.create_default_context()
-wrapped_socket=context.wrap_socket(mysocket,server_hostname='www.alphavantage.co')
+        context=ssl.create_default_context()
+        wrapped_socket=context.wrap_socket(mysocket,server_hostname='www.alphavantage.co')
 
-def send_cmd():
-    cmd=f'GET https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=NFLX&apikey={MY_API_KEY} HTTP/1.1\r\nHost: www.alphavantage.co\r\nConnection: close\r\n\r\n'.encode()
-    wrapped_socket.send(cmd)
-
-
-cmd=f'GET https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=NFLX&apikey={MY_API_KEY} HTTP/1.1\r\nHost: www.alphavantage.co\r\nConnection: close\r\n\r\n'.encode()
-wrapped_socket.send(cmd)
-
-while True:
-    data=wrapped_socket.recv(512)
-    if len(data) < 1:
         cmd=f'GET https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=NFLX&apikey={MY_API_KEY} HTTP/1.1\r\nHost: www.alphavantage.co\r\nConnection: close\r\n\r\n'.encode()
         wrapped_socket.send(cmd)
-        data=wrapped_socket.recv(512)
-        # wrapped_socket.send(cmd)
-        # print(f'test')
-        
-        # break
-    else:
-        # print(f'sleeping')
-        print(data.decode(), end='') 
-        
-        # time.sleep(5)
 
-wrapped_socket.close()
-mysocket.close()
+        while True:
+            data=wrapped_socket.recv(512)
+            if len(data) < 1:
+                # wrapped_socket.send(cmd)
+                # print(f'test')
+                break
+            else:
+                # print(f'sleeping')
+                print(data.decode(), end='') 
+                
+                # time.sleep(5)
+
+        wrapped_socket.close()
+        mysocket.close()
